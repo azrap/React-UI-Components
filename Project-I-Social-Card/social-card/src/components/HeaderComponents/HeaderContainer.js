48ba67663eb1c7068476605c75b701a0a79fae74
azrap <azraap@gmail.com>
2019-04-22 16:34:44 -0700

    putting in code for Header

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+
+import ImageThumbnail from './ImageThumbnail.js';
+
+function HeaderContainer (){
+    
+    return (
+        <div className="header-container">
+
+        <ImageThumbnail />
+         </div>
+    );
+}
+
+export default HeaderContainer
+
+
+{/* <CardContainer />
+        <CardBanner />
+        <CardContent />
+        */}
