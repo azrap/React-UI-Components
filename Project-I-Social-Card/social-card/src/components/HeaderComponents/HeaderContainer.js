46e969c2347d3642a6324b6a8e0e709b46d892e5
azrap <azraap@gmail.com>
2019-04-22 18:25:23 -0700

    got react code in forthe entire HeaderContainer.js including ImageThumbnail.js, HeaderContent.js, HeaderTitle.js

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './Header.css';
 
 import ImageThumbnail from './ImageThumbnail.js';
+import HeaderTitle from './HeaderTitle.js';
+import HeaderContent from './HeaderContent.js';
 
 function HeaderContainer (){
     
     return (
         <div className="header-container">
-
+        
         <ImageThumbnail />
+        <HeaderTitle />
+        <HeaderContent/>
          </div>
     );
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,5 +1,16 @@
 import React from 'react';
 import './Header.css';
 
+const HeaderTitle = () => {
+    return (
+        <div class='header-title'>
+         <h4>Lambda School</h4>   
+         <p> @LambdaSchool . 26 jan</p>
+        </div>
+    )
+}
 
-<HeaderContainer />
+
+
+
+export default HeaderTitle
